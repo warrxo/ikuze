9a9ea07c2bc73ff4ee9b4a6afeb7ea27a997d3ff
warrxo <ikuzee@yahoo.com>
2024-08-22 12:25:54 +0530

    menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,10 +2,12 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
+    pattern: "menu",
+    desc: "get cmd list",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+
+}catch(e){
